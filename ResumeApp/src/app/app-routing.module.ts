a42041385bee5815ec0ca00ff5b4aa2f9439ed60
Prakhar Khare <Prakhar.Khare@tatacommunications.com>
2019-04-13 13:21:07 +0530

    footer corrected

--- ResumeApp/src/app/app-routing.module.ts
+++ ResumeApp/src/app/app-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent }
+  { 
+    path: '', component: HomeComponent 
+},
+  { 
+    path: 'home', component: HomeComponent 
+},
+{
+  path: 'contact', component: ContactComponent 
+}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ResumeApp/src/app/app.module.ts
+++ ResumeApp/src/app/app.module.ts
@@ -7,19 +7,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { ContactComponent } from './contact/contact.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    ContactComponent
     
   ],
   imports: [
+
 
   
   BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
+    
     
   ],
   providers: [],
